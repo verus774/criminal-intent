5f8d6733885e256f900766ba946c2683cd950bab
Sergey Ladutko <verus774@mail.ru>
2017-02-05 21:41:03 +0300

    initial commit

---
+++ app/src/main/java/by/verus/criminalintent/CrimeActivity.java
@@ -0,0 +1,13 @@
+package by.verus.criminalintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
