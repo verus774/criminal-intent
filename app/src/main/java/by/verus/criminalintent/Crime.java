e298382532b2a6276ce6aca892e310e8b070800c
Sergey Ladutko <verus774@mail.ru>
2017-02-06 00:25:12 +0300

    some fragments added

--- app/src/main/java/by/verus/criminalintent/Crime.java
+++ app/src/main/java/by/verus/criminalintent/Crime.java
@@ -7,6 +7,7 @@ import com.activeandroid.annotation.Table;
 import com.activeandroid.query.Delete;
 import com.activeandroid.query.Select;
 
+import java.util.Date;
 import java.util.List;
 
 @Table(name = "crimes")
@@ -15,15 +16,25 @@ public class Crime extends Model {
     @Column(name = "title")
     private String mTitle;
 
+    @Column(name = "date", index = true)
+    private Date mDate;
+
+    @Column(name = "solved")
+    private boolean mSolved;
+
+
     public Crime() {
         super();
+        this.mDate = new Date();
     }
 
     public Crime(String title) {
         super();
 
         this.mTitle = title;
+        this.mDate = new Date();
     }
+
 
     public String getTitle() {
         return mTitle;
@@ -32,6 +43,23 @@ public class Crime extends Model {
     public void setTitle(String title) {
         mTitle = title;
     }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
 
     public static List<Crime> getAll() {
         return new Select()
---
+++ app/src/main/java/by/verus/criminalintent/CrimeListFragment.java
@@ -0,0 +1,27 @@
+package by.verus.criminalintent;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        return view;
+    }
+
+}
